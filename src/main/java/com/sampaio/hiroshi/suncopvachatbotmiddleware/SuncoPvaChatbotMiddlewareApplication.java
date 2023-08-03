01bed43a1919cbd44ffab3493e10f2fc5599e422
Hiroshi Sampaio <17440771+hiroshi-sampaio@users.noreply.github.com>
2023-08-03 18:18:01 -0300

    Created Spring Boot application

---
+++ src/main/java/com/sampaio/hiroshi/suncopvachatbotmiddleware/SuncoPvaChatbotMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.sampaio.hiroshi.suncopvachatbotmiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuncoPvaChatbotMiddlewareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuncoPvaChatbotMiddlewareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sampaio/hiroshi/suncopvachatbotmiddleware/SuncoPvaChatbotMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampaio.hiroshi.suncopvachatbotmiddleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuncoPvaChatbotMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
